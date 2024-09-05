dcef4a8e248834cda865bb7ca040219276752a79
Kat Tannehill <katetannehill@gmail.com>
2024-09-05 16:16:07 -0600

    Standardize ad data

--- src/App.js
+++ src/App.js
@@ -12,8 +12,28 @@ function App() {
   const fetchData = () => {
     fetch("http://localhost:3000/fakeDataSet")
     .then(resp => resp.json())
-    .then(setAds)
+    .then(data => setAds(standardizeAds(data)))
+    .catch(err => console.log("Error fetching data:", err))
   }
+
+  const standardizeAds = (data) => {
+    const allAds = [
+      ...(data.facebook_ads || []),
+      ...(data.google_analytics || []),
+      ...(data.snapchat_ads || []),
+      ...(data.twitter_ads || [])
+    ];
+  
+    return allAds.map(ad => ({
+      campaign: ad.campaign_name || ad.campaign || ad.utm_campaign,
+      adset: ad.media_buy_name || ad.ad_group || ad.ad_squad_name || ad.utm_medium,
+      creative: ad.ad_name || ad.image_name || ad.creative_name || ad.utm_content,
+      spend: ad.spend || ad.cost || 0, 
+      impressions: ad.impressions || 0, 
+      clicks: ad.clicks || ad.post_clicks || 0, 
+      results: ad.results || 0, 
+    }));
+  };
 
   console.log(ads)
 
