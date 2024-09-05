fcce01a6aa11c6e9412332172d2dcf580bcb9e39
Kat Tannehill <katetannehill@gmail.com>
2024-09-05 16:53:11 -0600

    Create Card component

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
-import logo from './logo.svg';
-import './App.css';
 import { useEffect, useState } from 'react';
+import Card from './components/Card'
 
 function App() {
   const [ads, setAds] = useState([])
@@ -37,22 +36,11 @@ function App() {
 
   console.log(ads)
 
+  const mappedAds = ads.map(ad => <Card key={`${ad.campaign_name}-${ad.ad_name}-${ad.creative_name}-${ad.index}`} ad={ad} />)
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      {mappedAds}
     </div>
   );
 }
---
+++ src/components/Card.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Card = (ad) => {
+
+    console.log(ad)
+
+  return (
+    <div>
+        
+    </div>
+  )
+}
+
+export default Card
