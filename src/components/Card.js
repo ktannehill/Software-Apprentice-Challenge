7ef6e8134842476a716d4b37e65969a8daf394f8
Kat Tannehill <katetannehill@gmail.com>
2024-09-05 16:56:44 -0600

    Populate Ad Cards

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,12 +1,19 @@
 import React from 'react'
 
-const Card = (ad) => {
+const Card = ({ ad }) => {
+    const { campaign, adset, creative, spend, impressions, clicks, results } = ad
 
-    console.log(ad)
+    console.log(campaign)
 
   return (
     <div>
-        
+        <h2>Campaign: {campaign}</h2>
+        <h3>Adset: {adset}</h3>
+        <p>Creative: {creative}</p>
+        <p>Spend: {spend}</p>
+        <p>Impressions: {impressions}</p>
+        <p>Clicks: {clicks}</p>
+        <p>Results: {results}</p>
     </div>
   )
 }
